9e8cf565674c48f5dff25752e947d1ebb9c5f20c
Matias Vallejos <matiavallejo@gmail.com>
2024-12-25 15:35:03 -0300

    build: frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  base: '/SaP-take-home/',
   server: {
     port: 4000,
   },
